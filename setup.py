2bec1cce05f55cb7029effd4f274859baa263fc1
Carlos676 <65645733+Carlos676@users.noreply.github.com>
2023-06-11 22:19:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ import re
 
 import setuptools
 
-readme=open(".README.md","r")
 setuptools.setup(
     name = "saludo",
     packages = ['saludo'],
     version = '0.1',
     description = 'Prueba de libreria de saludo',
-    long_description=readme.read(),
     long_description_contenr_type='text/markdown',
     author = 'Carlos',
     author_email = 'juancarlosemp600a@gmail.com',
-    url = 'https://github.com/Carlos676/saludo.git',
+    url = 'https://github.com/Carlos676/saludo',
     license = 'MIT',    
     classifiers = [
         'Prueba de libreria',
