a9189e7d02b4c0a4de769e918cfdfcd798faaeaf
carlos mamani <juancarlosmamanimamani011@gmail.com>
2023-06-11 21:55:26 -0400

    prueba de libreria 2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Este archivo describe el siguiente proyecto
 # esta el archivo saludo
+
+'''
+from libreria_carlos import Saludo
+saludo = Saludo(nombre='Juan Carlos Mamani')
+print(saludo.saludo_carlos())
+
+'''
--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import re
 
 import setuptools
 
+readme=open(".README.md","r")
 setuptools.setup(
     name = "saludo",
+    packages = ['saludo'],
     version = '0.1',
     description = 'Prueba de libreria de saludo',
-    url = 'https://github.com/Carlos676/saludo',
+    long_description=readme.read(),
+    long_description_contenr_type='text/markdown',
     author = 'Carlos',
     author_email = 'juancarlosemp600a@gmail.com',
-    license = 'MIT',
-    packages = ['saludo'],
-    
+    url = 'https://github.com/Carlos676/saludo',
+    license = 'MIT',    
     classifiers = [
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent', 
-    ],
-    zip_safe = False,
+        'Prueba de libreria',
+        ],
+    include_package_data=TR,
 )
